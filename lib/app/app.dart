7c847a25c3e3825373aada29b5e933f3279bf4e5
Utsavraut <raututsav35@gmail>
2023-12-06 09:22:37 +0545

    Calculator App

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+// import 'package:calculator_app/routes/app_routes.dart';
+// import 'package:flutter/material.dart';
+
+// class App extends StatelessWidget{
+//   const App({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter_batch31',
+//       initialRoute: AppRoute.dashboardRoute,
+//       routes: AppRoute.getApplicationRoute(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,12 @@
+// import 'package:calculator_app/view/calculator_view.dart';
+
+// class AppRoute{
+//   AppRoute._();
+//    static const String calcRoute = '/';
+
+//    static getApplicationRoute() {
+//     return{
+//       calcRoute: (context) => const CalculatorView(),
+//     };
+//    }
+// }
